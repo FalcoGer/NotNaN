3c18085e13d2db316523a195fec1da911ca5e1f3
FalcoGer <16250196+FalcoGer@users.noreply.github.com>
2025-02-11 08:10:40 +0100

    i give up. replaces print with format and cout

--- main.cpp
+++ main.cpp
@@ -1,41 +1,41 @@
 #include "NotNaN.hpp"
 #include <iostream>
 #include <limits>
-#include <print>
+#include <format>
 
 auto main() -> int
 {
     // NOLINTBEGIN(readability-magic-numbers) // example code
 
     NotNaN f1 {12.F};
-    std::println("{:>+7.2F}", f1);
+    std::cout << std::format("{:>+7.2F}", f1) << '\n';
     std::cout << f1 << '\n';
     auto f2  = f1 + 10.0;
     f2      += 1.0;
-    std::println("{}", f2);
-    std::println("{}", f2 + 3 == 26.F);
+    std::cout << std::format("{}", f2) << '\n';
+    std::cout << std::format("{}", f2 + 3 == 26.F) << '\n';
 
     f2 -= 20.F;
-    std::println("{}", f2);
+    std::cout << std::format("{}", f2) << '\n';
 
     f2 *= 2.F;
-    std::println("{}", f2);
+    std::cout << std::format("{}", f2) << '\n';
 
     auto f3 = 10 / f2;
-    std::println("{}", f3);
+    std::cout << std::format("{}", f3) << '\n';
 
     const float  INF = std::numeric_limits<float>::infinity();
     NotNaN f4 {INF};
-    std::println("{}", f4);
+    std::cout << std::format("{}", f4) << '\n';
 
     try
     {
         auto f5 = f4 - f4;
-        std::println("{}", f5);
+        std::cout << std::format("{}", f5) << '\n';
     }
     catch (const std::exception& ex)
     {
-        std::println("Exception: {}", ex.what());
+        std::cout << std::format("Exception: {}", ex.what()) << '\n';
     }
     // NOLINTEND(readability-magic-numbers)
 }
